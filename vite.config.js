8656ea6c18b02196e2e5260b0e6861a2c6636236
aland-dev <126734529+aland-dev@users.noreply.github.com>
2024-03-02 22:39:24 +0300

    refactor: remix-vite is now stable 🥳🎉

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import jsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from "@remix-run/dev"
+import { defineConfig } from "vite"
+import jsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [remix(), jsconfigPaths()],
   server: {
     open: true,
   },
-});
+})
